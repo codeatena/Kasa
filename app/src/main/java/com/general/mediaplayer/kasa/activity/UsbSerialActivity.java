3944f732e596cdc77a83ff78815dcb5b495f52a3
codeatena <codeatena@yahoo.com>
2017-06-26 05:16:51 +0800

    update code

--- app/src/main/java/com/general/mediaplayer/kasa/activity/UsbSerialActivity.java
+++ app/src/main/java/com/general/mediaplayer/kasa/activity/UsbSerialActivity.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
@@ -29,7 +30,7 @@ public class UsbSerialActivity extends BaseActivity {
     private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
     private SerialInputOutputManager mSerialIoManager;
 
-    private final SerialInputOutputManager.Listener mListener =
+    public final SerialInputOutputManager.Listener mListener =
             new SerialInputOutputManager.Listener() {
 
                 @Override
@@ -39,21 +40,13 @@ public class UsbSerialActivity extends BaseActivity {
 
                 @Override
                 public void onNewData(final byte[] data) {
-                    UsbSerialActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            Log.i(TAG, data.toString());
-
-                        }
-                    });
+                    Log.v(TAG, data.toString());
                 }
             };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
         List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
@@ -73,19 +66,19 @@ public class UsbSerialActivity extends BaseActivity {
         if (sPort != null) {
 
             final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-            UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
-            if (connection == null) {
+            if (usbManager.hasPermission(sPort.getDriver().getDevice())){
+
+                UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
+                openConnection(connection);
+                onDeviceStateChange();
+            }
+            else{
 
                 PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
                 IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
                 registerReceiver(mUsbReceiver, filter);
                 usbManager.requestPermission(sPort.getDriver().getDevice(), mPermissionIntent);
-
-                return;
             }
-
-            openConnection(connection);
-            onDeviceStateChange();
         }
     }
 
@@ -150,17 +143,35 @@ public class UsbSerialActivity extends BaseActivity {
         }
     }
 
-    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+    public final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
 
             String action = intent.getAction();
             if (ACTION_USB_PERMISSION.equals(action)) {
+//                synchronized (this) {
+//
+//                    final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+//                    UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
+//                    openConnection(connection);
+//                    onDeviceStateChange();
+//
+//                }
                 synchronized (this) {
 
-                    final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-                    UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
-                    openConnection(connection);
+                    UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if(device != null){
+                            //call method to set up device communication
+                            final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+                            UsbDeviceConnection connection = usbManager.openDevice(device);
+                            openConnection(connection);
+                            onDeviceStateChange();
+                        }
+                    }
+                    else {
+                        Log.d(TAG, "permission denied for device " + device);
+                    }
                 }
             }
 
